68892a67736affa7086df009fef5db712626971a
mauloajin <mauloa32153@gmail.com>
2026-07-22 11:45:08 +0900

    Add app/robots.js

---
+++ app/robots.js
@@ -0,0 +1,2 @@
+export const dynamic = 'force-static';
+export default function robots(){return {rules:{userAgent:'*',allow:'/'},sitemap:'https://operetta52.vercel.app/sitemap.xml',host:'https://operetta52.vercel.app'}}
