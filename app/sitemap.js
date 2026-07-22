4608f00b12a9e54f179474d6ec3b58281bcba302
mauloajin <mauloa32153@gmail.com>
2026-07-22 11:45:09 +0900

    Add app/sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,2 @@
+export const dynamic = 'force-static';
+export default function sitemap(){return [{url:'https://operetta52.vercel.app/',lastModified:new Date(),changeFrequency:'weekly',priority:1}]}
